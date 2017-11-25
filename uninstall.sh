015d572e868849aeda4c9652238abf5beb9d5c09
moxin <moxin@x0.fios-router.home>
2017-11-25 11:24:39 -0500

    update

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,14 @@
-
+msg(){
+    echo -e "Dotfiles: $*"
+}
 
 confirmation=$(bash -c 'read -r -p "Are you sure you want to remove dotfiles? [y/N]: " tmp; echo $tmp')
 if [ "$confirmation" != y ] && [ "$confirmation" != Y ]; then
-  echo "Uninstall cancelled"
+  msg "Uninstall cancelled"
   exit
 fi
 
-echo "Removing ~/.dotfiles"
+msg "Removing ~/.dotfiles"
 if [ -d ~/.dotfiles ]; then
   rm -rf ~/.dotfiles
 fi
@@ -23,25 +25,25 @@ for dotfile in ${dotfiles[@]}
 do
     pre_dotfile="${HOME}/.${dotfile}.pre"
     if [ -f $pre_dotfile ] || [ -h $pre_dotfile ]; then
-        echo "Found ${pre_dotfile} -- Restoring to .${dotfile}"
+        msg "Found ${pre_dotfile} -- Restoring to .${dotfile}"
         if [ -f .$dotfile ] || [ -h .$dotfile ]; then 
             dotfile_to_save=".dotfile.$dotfile-uninstalled-$(date +%Y%m%d%H%M%S)";
-            echo "Found ~/.$dotfile -- Renaming to ~/${dotfile_to_save}";
+            msg "Found ~/.$dotfile -- Renaming to ~/${dotfile_to_save}";
             mv ~/.$dotfile ~/"${dotfile_to_save}";
         fi  
         
         mv $pre_dotfile ${HOME}/.$dotfile
-        echo "Your original $dotfile was restored."
+        msg "Your original $dotfile was restored."
     fi
 done
 
 
 #restore to bash
 if hash chsh >/dev/null 2>&1; then
-    echo "Switching back to bash"
+    msg "Switching back to bash"
     chsh -s /bin/bash
 else
-    echo "You can edit /etc/passwd to switch your default shell back to bash"
+    msg "You can edit /etc/passwd to switch your default shell back to bash"
 fi
 
-echo "Dotfiles has been uninstalled."
+msg "[✔] Dotfiles has been uninstalled."

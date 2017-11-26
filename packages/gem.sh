42c417da63c167c12e50e5d1fca052b1ffa2edbf
xta0 <jayson.xu@foxmail.com>
2017-11-26 07:05:04 +0100

    update

--- packages/gem.sh
+++ packages/gem.sh
@@ -1,12 +1,16 @@
 #Install RVM
-brew install gnupg
-gpg --keyserver hkp://keys.gnupg.net
-    --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+brew install gpg
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
 
 #rvm
 export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+#install ruby
+rvm install ruby-2.4.1
+rvm --default use 2.4.1
 
 #Install Gems
 gem install bundler

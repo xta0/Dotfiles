6bfbacbfff112669368fa0b93d10889be0ae305f
Tao Xu <taox@fb.com>
2024-12-28 16:59:03 -0800

    Update(auto commit)

--- packages/conda.sh
+++ packages/conda.sh
@@ -4,4 +4,4 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm ~/miniconda3/miniconda.sh
 export PATH="~/miniconda3/bin:${PATH}"
 source ~/miniconda3/bin/activate
-conda init --all
+conda init zsh
--- packages/rvm.sh
+++ packages/rvm.sh
@@ -1,7 +1,7 @@
 #Install RVM
-brew install gpg
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+
 \curl -sSL https://get.rvm.io | bash -s stable
 
 #rvm
@@ -9,13 +9,9 @@ export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 #install ruby
-rvm install ruby --latest
-rvm --default use 3.0.0
+rvm install ruby-3.2.2 --with-openssl-dir=/opt/homebrew/opt/openssl@1.1/
+rvm --default use 3.2.2
 
 #Install Gems
 gem install bundler
-gem install rake
-gem install rack
-gem install nokogiri
 gem install jekyll
-gem install cocoapods

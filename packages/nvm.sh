1da8f2f0274c6056c2e5c52a37b1376a98da1b69
1PSDK User <1psdk@1psdk.com>
2024-12-28 16:29:21 -0800

    Update(auto commit)

--- packages/nvm.sh
+++ packages/nvm.sh
@@ -1,6 +1,6 @@
 
 #Install NVM
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.40.1/install.sh | bash
 
 #Export NVM
 export NVM_DIR="$HOME/.nvm"
